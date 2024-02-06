245a0332812333d0d1785d59cd4fa3395ab777c7
dev-chpark <xpmxf5@gmail.com>
2024-02-06 14:16:48 +0900

    ⭐ feat: Calculator 에 덧셈 연산 구현

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Calculator {
+    public double calculate(String input) {
+        String[] parts = input.split(" ");
+        double operand1 = Double.parseDouble(parts[0]);
+        String operator = parts[1];
+        double operand2 = Double.parseDouble(parts[2]);
+
+        switch (operator) {
+            case "+" :
+                return operand1 + operand2;
+            default:
+                throw new IllegalArgumentException("Unsupported Operation");
+        }
+    }
+}
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,4 @@
+import org.example.Calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
