eddd759ecfe9e5b3307b7deb16386dd7e7614f4e
dev-chpark <xpmxf5@gmail.com>
2024-02-06 14:13:46 +0900

    🚦 test: 덧셈에 대한 테스트 케이스 추가

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.calculate("2 + 3"), "2 + 3 = 5");
+    }
+}
