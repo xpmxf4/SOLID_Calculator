dd35a2d2cada017ba547e9c519199093f9d09016
dev-chpark <xpmxf5@gmail.com>
2024-02-26 13:41:23 +0900

    🛠️  config: mockito 추가

--- src/test/java/util/InputParserTest.java
+++ src/test/java/util/InputParserTest.java
@@ -1,22 +1,6 @@
 package util;
 
-import org.example.dto.CalculateDto;
-import org.example.util.InputParser;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class InputParserTest {
-
-    @Test
-    @DisplayName("parsing 이 제대로 됐다면 계산도 제대로 된다.")
-    void testParseOperandsForBinaryOperation() {
-        String input = "3 + 5";
-        CalculateDto calculateDto = InputParser.parseCommandLine(input);
-        double expectedRes = 8.0;
-
-        assertEquals(expectedRes, calculateDto.calculate(), "Should correctly parse operands for binary operation");
-    }
 
 }
