c927be52c62819971e32036928cb978e890ce578
dev-chpark <xpmxf5@gmail.com>
2024-02-26 13:31:16 +0900

    ♻️ refactor: InputParser 를 함수형 인터페이스와 맵을 사용하는 방식으로 refactor

--- src/main/java/org/example/util/InputParser.java
+++ src/main/java/org/example/util/InputParser.java
@@ -4,35 +4,50 @@ import org.example.operation.Operation;
 import org.example.operation.binary.*;
 import org.example.operation.unary.UnaryOperator;
 
+import java.util.Map;
+
 public class InputParser {
+    private InputParser() {
+    }
 
-    public static double parseCommandLine(String input) {
-        String[] parts = input.split(" ");
-        int splitInputLen = parts.length;
-
-        try {
-            if (splitInputLen == 2) {
+    static Map<Integer, OperationHandler> handlers = Map.of(
+            2, parts -> {
                 String operationType = parts[0];
                 double operand = Double.parseDouble(parts[1]);
                 Operation op = OperationRegistry.getOperation(operationType);
 
-                if (op instanceof UnaryOperator unaryOperator) {
-                    return unaryOperator.calculate(operand);
+                if (op instanceof UnaryOperator unaryOp) {
+                    return unaryOp.calculate(operand);
+                } else {
+                    throw new IllegalArgumentException("Not UnaryOperation");
                 }
-            } else if (splitInputLen == 3) {
+            },
+            3, parts -> {
                 double operand1 = Double.parseDouble(parts[0]);
                 String operationType = parts[1];
                 double operand2 = Double.parseDouble(parts[2]);
                 Operation op = OperationRegistry.getOperation(operationType);
 
-                if (op instanceof BinaryOperator binaryOperator) {
-                    return binaryOperator.calculate(operand1, operand2);
+                if (op instanceof BinaryOperator binaryOp) {
+                    return binaryOp.calculate(operand1, operand2);
+                } else {
+                    throw new IllegalArgumentException("Not BinaryOperation");
                 }
             }
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Invalid operand: " + input, e);
-        }
+    );
 
-        throw new IllegalArgumentException("Invalid input format or operation type: " + input);
+    public static double parseCommandLine(String input) {
+        String[] parts = input.split(" ");
+        OperationHandler handler = handlers.get(parts.length);
+
+        if (handler != null)
+            return handler.handle(parts);
+        else
+            throw new IllegalArgumentException("Invalid Input");
     }
+}
+
+@FunctionalInterface
+interface OperationHandler {
+    double handle(String[] parts);
 }
